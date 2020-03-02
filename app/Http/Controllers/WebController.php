4ab86e608ce1606bf953237361c02504fdfd74ab
phamthai12316 <phamthai12316@gmail.com>
2020-03-02 12:45:23 +0700

    fix

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -10,13 +10,13 @@ class WebController extends Controller
 
 {
     public function home(){
-        return view('resources/views/index');
+        return view('index');
     }
     public function product(){
-        return view('resources/views/product');
+        return view('product');
     }
     public function productDetail(){
-        return view('resources/views/productDetail');
+        return view('productDetail');
     }
 }
 
