0a03e5938e90b87eba8848eb1db12fcd0054c35b
phamthai12316 <phamthai12316@gmail.com>
2020-03-08 04:29:42 +0700

    Change file

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -12,9 +12,9 @@ class WebController extends Controller
 {
 
     public function home(){
-        $h1 = Product::orderBy('product_name','asc')->take(3)->get();
+        $h1 = Product::orderBy('created_at','desc')->take(3)->get();
         $h2 = Product::orderBy('product_desc','asc')->take(3)->get();
-        $h3 = Product::find('gallery')->take(3)->get();
+        $h3 = Product::orderBy('gallery')->take(3)->get();
 
         return view('index',['h1'=>$h1,'h2'=>$h2,'h3'=>$h3]);
     }
