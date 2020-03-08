df661700677f64c485099effd7066e6cc7708673
phamthai12316 <phamthai12316@gmail.com>
2020-03-08 12:11:20 +0700

    Change file

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -162,33 +162,35 @@
     <section id="features">
         <div class="container">
             <div class="row">
+                @foreach($h1 as $p)
                 <div class="single-features">
                     <div class="col-sm-5 wow fadeInLeft" data-wow-duration="500ms" data-wow-delay="300ms">
-                        <img src="{{$h1 -> thumbnail}}" class="img-responsive" alt="">
+                        <img src="{{$p -> thumbnail}}" class="img-responsive" alt="">
                     </div>
                     <div class="col-sm-6 wow fadeInRight" data-wow-duration="500ms" data-wow-delay="300ms">
-                        <h2>{{$h1 -> product_name}}</h2>
-                        <P>{{$h1 -> product_desc}}</P>
+                        <h2>{{$p -> product_name}}</h2>
+                        <P>{{$p -> product_desc}}</P>
                     </div>
                 </div>
-                <div class="single-features">
-                    <div class="col-sm-6 col-sm-offset-1 align-right wow fadeInLeft" data-wow-duration="500ms" data-wow-delay="300ms">
-                        <h2>{{$h1 -> product_name}}</h2>
-                        <P>Mollit eiusmod id chuck turducken laboris meatloaf pork loin tenderloin swine. Pancetta excepteur fugiat strip steak tri-tip. Swine salami eiusmod sint, ex id venison non. Fugiat ea jowl cillum meatloaf.</P>
-                    </div>
-                    <div class="col-sm-5 wow fadeInRight" data-wow-duration="500ms" data-wow-delay="300ms">
-                        <img src="{{$h1 -> thumbnail}}" class="img-responsive" alt="">
-                    </div>
-                </div>
-                <div class="single-features">
-                    <div class="col-sm-5 wow fadeInLeft" data-wow-duration="500ms" data-wow-delay="300ms">
-                        <img src="{{$h1 -> thumbnail}}" class="img-responsive" alt="">
-                    </div>
-                    <div class="col-sm-6 wow fadeInRight" data-wow-duration="500ms" data-wow-delay="300ms">
-                        <h2>{{$h1 -> product_name}}</h2>
-                        <P>Ut officia cupidatat anim excepteur fugiat cillum ea occaecat rump pork chop tempor. Ut tenderloin veniam commodo. Shankle aliquip short ribs, chicken eiusmod exercitation shank landjaeger spare ribs corned beef.</P>
-                    </div>
-                </div>
+                @endforeach
+{{--                <div class="single-features">--}}
+{{--                    <div class="col-sm-6 col-sm-offset-1 align-right wow fadeInLeft" data-wow-duration="500ms" data-wow-delay="300ms">--}}
+{{--                        <h2>{{$h1 -> product_name}}</h2>--}}
+{{--                        <P>Mollit eiusmod id chuck turducken laboris meatloaf pork loin tenderloin swine. Pancetta excepteur fugiat strip steak tri-tip. Swine salami eiusmod sint, ex id venison non. Fugiat ea jowl cillum meatloaf.</P>--}}
+{{--                    </div>--}}
+{{--                    <div class="col-sm-5 wow fadeInRight" data-wow-duration="500ms" data-wow-delay="300ms">--}}
+{{--                        <img src="{{$h1 -> thumbnail}}" class="img-responsive" alt="">--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--                <div class="single-features">--}}
+{{--                    <div class="col-sm-5 wow fadeInLeft" data-wow-duration="500ms" data-wow-delay="300ms">--}}
+{{--                        <img src="{{$h1 -> thumbnail}}" class="img-responsive" alt="">--}}
+{{--                    </div>--}}
+{{--                    <div class="col-sm-6 wow fadeInRight" data-wow-duration="500ms" data-wow-delay="300ms">--}}
+{{--                        <h2>{{$h1 -> product_name}}</h2>--}}
+{{--                        <P>Ut officia cupidatat anim excepteur fugiat cillum ea occaecat rump pork chop tempor. Ut tenderloin veniam commodo. Shankle aliquip short ribs, chicken eiusmod exercitation shank landjaeger spare ribs corned beef.</P>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
             </div>
         </div>
     </section>
